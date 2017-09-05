c2d13d28d6a17ac77968fc250725f728fbf73a52
Øyvind Kallstad <kallstad@gmail.com>
2017-09-05 14:04:01 +0200

    Add more tests

--- test.ps1
+++ test.ps1
@@ -3,6 +3,7 @@
     Block comment
 #>
 'Single Quoted string'
+"Double Quoted string"
 7
 -10lkb
 0x9
@@ -16,3 +17,36 @@
 4.5
 -9.9
 +7.4
+$myVariable
+$my-Variable
+$local:myVariable
+$global:myVariable
+$script:myVariable
+$private:myVariable
+$using:myVariable
+$env:COMPUTERNAME
+$Function:help
+$Alias:cat
+$Cert:myCert
+$Variable:ast
+$HKCU:System
+$HKLM:System
+$WSMan:localhost
+${myVariable}
+${my-Variable}
+${local:myVariable}
+${global:myVariable}
+${script:myVariable}
+${private:myVariable}
+${using:myVariable}
+${env:COMPUTERNAME}
+${Function:help}
+${Alias:cat}
+${Cert:myCert}
+${Variable:ast}
+${HKCU:System}
+${HKLM:System}
+${WSMan:localhost}
+command.exe $par1 --% $par2 #comment
+$$
+'''str''ing'
