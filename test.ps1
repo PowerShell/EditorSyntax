033f4bb6c1548fef6b0ab2629cb73d33e6d19060
Øyvind Kallstad <kallstad@gmail.com>
2017-09-06 13:32:54 +0200

    Lots more tests added

--- test.ps1
+++ test.ps1
@@ -66,3 +66,49 @@ class myClass {}
 1 -as [string]
 1 -isnot [int]
 "c" -in [char[]]"abc"
+"c" -notin [char[]]"abc"
+1,2 -join ";"
+1,2 -split ","
+1 -eq 2
+1 -ne 2
+1 -gt 2
+1 -lt 2
+1 -ge 2
+1 -le 2
+1 -like 2
+1 -notlike 2
+"abc" -replace("b","c")
+1,2,3 -contains(2)
+1,2,3 -notcontains(2)
+"a" -match("\w")
+"a" -notmatch("\w")
+102 -shl 2
+102 -shr 2
+5 -band 3
+-bnot 5
+5 -bor 0x03
+5 -bxor 3
+(1 -gt 2) -and (2 -gt 1)
+(1 -gt 2) -or (2 -gt 1)
+(1 -eq 1) -xor (2 -eq 2)
+-not (1 -eq 1)
+!(1 -eq 1)
+"{0} {1,-10} {2:N}" -f 1,"hello",[math]::pi
+$a = 1
+$a += 1
+$a -= 1
+$a *= 1
+$a /= 1
+$a %= 1
+$a++
+++$a
+$a--
+--$a
+2*2
+2/2
+2+2
+2-2
++$a
+-$a
+1..4
+& $a
