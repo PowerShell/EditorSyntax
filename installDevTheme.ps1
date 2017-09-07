0474c8edb7a91c96b4d045cb5e0944a5092b4f0f
Øyvind Kallstad <kallstad@gmail.com>
2017-09-07 16:40:06 +0200

    Update path

--- installDevTheme.ps1
+++ installDevTheme.ps1
@@ -1,4 +1,4 @@
 $sourcePath = '.\themes\theme-psise\theme.json'
-$destinationPath = Join-Path -Path $env:USERPROFILE -ChildPath '.vscode\extensions\ms-vscode.powershell-1.4.1\themes\theme-psise'
+$destinationPath = Join-Path -Path $env:USERPROFILE -ChildPath '.vscode\extensions\ms-vscode.powershell-1.4.2\themes\theme-psise'
 Copy-Item -Path $sourcePath -Destination $destinationPath -Force
 Write-Host 'Installed ISE Dev Theme'
