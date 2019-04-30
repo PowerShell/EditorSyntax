19f3fe46aaa83d7cbd3debd8227544d60db01e19
msftrncs <msftrncs@midlands.net>
2019-04-30 00:42:58 -0500

    improvements/changes relating to #169

--- tools/build-helpers.ps1
+++ tools/build-helpers.ps1
@@ -1,12 +1,13 @@
 function DownloadAtom {
     Write-Host "Downloading latest Atom release..."
 
-    $Source      = "https://atom.io/download/windows_zip?channel=stable"
+    $Source = "https://atom.io/download/windows_zip?channel=stable"
     $Destination = Join-Path . '\atom.zip'
 
     try {
         Invoke-WebRequest $Source -OutFile $Destination -ErrorAction Stop
-    } catch {
+    }
+    catch {
         $PSCmdlet.ThrowTerminatingError($PSItem)
     }
 
@@ -24,7 +25,8 @@ function ExtractAtom {
     try {
         Add-Type -AssemblyName System.IO.Compression.FileSystem
         [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFile, $OutPath)
-    } catch {
+    }
+    catch {
         $PSCmdlet.ThrowTerminatingError($PSItem)
     }
 
@@ -35,24 +37,27 @@ function ExtractAtom {
     Write-Host "Atom.zip deleted..."
 }
 
-function ParseJasmine ($String) {
-    if ($String -match "^\s+at.*$") {
-        ' '
-        return
-    }
+filter ParseJasmine {
+    switch -regex ($_) {
+        ^\s+at {
+            ''
+            break
+        }
 
-    if ($_ -match "^\s+it.*$") {
-        $_ -replace '^(\s+)(it)','$1[-] It'
-        return
-    }
+        ^\s+it {
+            $_ -replace '^(\s+)(it)', '$1[-] It'
+            break
+        }
 
-    if ($_ -match "^\s+Expected.*$") {
-        $x = $_ -replace '^(\s*)(Expected)(.*)\s(to equal .*)','$1$2$3%%$1$4'
-        $x.Replace('%%',"`n  ")
-        return
-    }
+        ^\s+Expected {
+            $_ -replace '^(\s*)(Expected.*?)\s(instead found .*)', "`$1`$2`n`$1`$3"
+            break
+        }
 
-    $_
+        default {
+            $_
+        }
+    }
 }
 
 function ExitWithCode {
@@ -70,15 +75,17 @@ function RunSpecs {
     $specpath = Join-Path . '\spec'
 
     if (Test-Path $specpath) {
-      & "$script:ATOM_EXE_PATH" --test spec *>&1 | ForEach-Object { ParseJasmine $_ }
-    } else {
+        & $script:ATOM_EXE_PATH --test $specpath *>&1 | ParseJasmine
+    }
+    else {
         throw '.\spec\ not found.'
     }
 
     if ($LASTEXITCODE -ne 0) {
         if ($Env:APPVEYOR) {
             ExitWithCode -exitcode $LASTEXITCODE
-        } else {
+        }
+        else {
             throw "One or more tests failed."
         }
     }
